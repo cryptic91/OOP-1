9d1ac29c3c5f262c1fe2250c1ba5ec232909fb17
Shanto <rakibul15-3871@diu.edu.bd>
2022-05-03 22:58:31 +0600

    As

---
+++ JAVA_Ecllipse/FInalL2T1/src/main/As.java
@@ -0,0 +1,15 @@
+package main;
+
+public class As extends Sam{
+	
+	public int id;
+	public String exam;
+	
+	public void run() {
+		System.out.println("Id: "+id+"    Exam: "+exam+"     ");
+//		System.out.println("My name "+getName());
+//		System.out.println("My age "+getAge());
+		System.out.println();
+	}
+	
+}
---
+++ JAVA_Ecllipse/Lieutenant/src/firstPackage/As.java
@@ -0,0 +1,32 @@
+package firstPackage;
+
+public class As {
+	
+	private String a;
+	private String b;
+	private String c;
+	
+	public String getA() {
+		return a;
+	}
+	public void setA(String a, int b, double c) {
+		this.a = a;
+	}
+	
+	public String getB() {
+		return b;
+	}
+	public void setB(String b) {
+		this.b = b;
+	}
+	
+	public String getC() {
+		return c;
+	}
+	public void setC(String c) {
+		this.c = c;
+	}
+	
+
+
+}
